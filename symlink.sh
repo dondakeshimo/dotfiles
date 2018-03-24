5224e43c1b4a65c99641d8a3f47b934ecb0a3663
Shimomura Taku <went.went.takkun135@gmail.com>
2018-03-24 22:36:35 +0900

    [add]atomのdotfilesも管理下に含めた

--- symlink.sh
+++ symlink.sh
@@ -6,3 +6,8 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.tmux ~/.tmux
 ln -sf ~/dotfiles/.jupyter ~/.jupyter
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/config.cson ~/.atom/config.cson
+ln -sf ~/dotfiles/init.coffee ~/.atom/init.coffee
+ln -sf ~/dotfiles/keymap.cson ~/.atom/keymap.cson
+ln -sf ~/dotfiles/snippets.cson ~/.atom/snippets.cson
+ln -sf ~/dotfiles/styles.less ~/.atom/styles.less
