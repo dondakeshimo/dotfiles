9f8adeeed25efad520eb6876f81d60b5cff7bc9c
Shimomura Taku <went.went.takkun135@gmail.com>
2018-06-15 00:04:51 +0900

    [fix]symlink.shを汎用的にした

--- symlink.sh
+++ symlink.sh
@@ -1,13 +1,18 @@
-#!/bin/sh
+for f in .??*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
 
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.tmux ~/.tmux
-ln -sf ~/dotfiles/.jupyter ~/.jupyter
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/config.cson ~/.atom/config.cson
-ln -sf ~/dotfiles/init.coffee ~/.atom/init.coffee
-ln -sf ~/dotfiles/keymap.cson ~/.atom/keymap.cson
-ln -sf ~/dotfiles/snippets.cson ~/.atom/snippets.cson
-ln -sf ~/dotfiles/styles.less ~/.atom/styles.less
+    ln -sf $PWD/$f ~/$f
+    echo "$PWD/$f"
+done
+
+for f in atom/*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+
+    echo "$f"
+    ln -sf $PWD/$f ~/.$f
+    echo "$f"
+done
