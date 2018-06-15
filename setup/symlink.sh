bf8c1517e0754021d61264175f4218201cdc7c96
Shimomura Taku <went.went.takkun135@gmail.com>
2018-06-16 00:58:24 +0900

    [add]tmux-builderを追加

--- setup/symlink.sh
+++ setup/symlink.sh
@@ -19,3 +19,12 @@ do
     ln -sf $PWD/$f ~/.$f
     echo "$f"
 done
+
+for f in bin/*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+
+    ln -sf $PWD/$f ~/Scripts/bin
+    echo "$f"
+done
