0fe50f44beb1027ce81223e0732f202f8a55964b
dondakeshimo <went.went.takkun135@gmail.com>
2019-08-29 23:44:40 +0900

    [fix]fix language manager error handling

--- .zsh/60_init_lang_mng.zsh
+++ .zsh/60_init_lang_mng.zsh
@@ -3,27 +3,37 @@
 ##############################
 
 # pyenv setting
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+if has "pyenv"; then
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+fi
 
 # nodebrew setting
-export PATH="$HOME/.nodebrew/current/bin:$PATH"
+if has "nodebrew"; then
+    export PATH="$HOME/.nodebrew/current/bin:$PATH"
+fi
 
 # gcc PATH setting for lightGBM
 # export PATH="/usr/local/Cellar/gcc/8.1.0/bin:$PATH"
 
 # Tex setting
-export PATH="/Library/TeX/texbin:$PATH"
+if has "tex"; then
+    export PATH="/Library/TeX/texbin:$PATH"
+fi
 
 # rbenv setting
-export RBENV_ROOT="$HOME/.rbenv"
-export PATH="$RBENV_ROOT/bin:$PATH"
-export PATH="$RBENV_ROOT/shims:$PATH"
-eval "$(rbenv init -)"
+if has "rbenv"; then
+    export RBENV_ROOT="$HOME/.rbenv"
+    export PATH="$RBENV_ROOT/bin:$PATH"
+    export PATH="$RBENV_ROOT/shims:$PATH"
+    eval "$(rbenv init -)"
+fi
 
 # direnv setting
-eval "$(direnv hook zsh)"
+if has "direnv"; then
+    eval "$(direnv hook zsh)"
+fi
 
 # use mysql 5.6 first
 export PATH="/usr/local/opt/mysql@5.6/bin:$PATH"
