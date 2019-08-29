c4f8f8650cfb698adc01f958c41eaf239efdde6a
dondakeshimo <went.went.takkun135@gmail.com>
2019-08-29 23:36:31 +0900

    [add]requirements_install_apt.sh

---
+++ setup/requirements_install_apt.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+has() {
+    type "${1:?too few arguments}" &>/dev/null
+}
+
+# confirm apt command exists
+if ! has "apt"; then
+    echo "apt is required"
+    exit 1
+fi
+
+
+# install requirements via homebrew
+sudo apt update
+sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+    libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+    xz-utils tk-dev libffi-dev liblzma-dev python-openssl git \
+    vim zsh tmux reattach-to-user-namespace golang
+
+
+# ghq setup
+if ! has "go"; then
+    echo "golang is something wrong"
+    exit 1
+fi
+mkdir "$HOME/Scripts"
+export GOPATH="$HOME/Scripts"
+export PATH="$GOHOME/Scripts/bin:$PATH"
+go get github.com/motemen/ghq
+echo '[ghq]\n\troot = $HOME/Scripts/src' >> $HOME/.gitconfig
+
+
+# install zplug
+if ! has "zsh"; then
+    echo "zsh is something wrong"
+    exit 1
+fi
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+
+# download my dotfiles
+if ! has "ghq"; then
+    echo "ghq is something wrong"
+    exit 1
+fi
+ghq get https://github.com/dondakeshimo/dotfiles.git
+
+
+# success message
+echo
+echo "Requirements installing is success!!"
+
+
