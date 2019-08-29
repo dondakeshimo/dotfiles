f308c1a9afbd0a7436222e85ddc6d0cb89f8adcc
dondakeshimo <went.went.takkun135@gmail.com>
2019-08-29 23:39:49 +0900

    [fix]fix error handling

--- setup/requirements_install_mac.sh
+++ setup/requirements_install_mac.sh
@@ -7,6 +7,7 @@ has() {
 # confirm here is osx
 if [[ "$(uname)" != "Darwin" ]]; then
     echo "This computer is not Mac"
+    exit 1
 fi
 
 
@@ -24,21 +25,38 @@ fi
 
 
 # install requirements via homebrew
+if ! has "brew"; then
+    echo "Homebrew is something wrong"
+    exit 1
+fi
 brew install git vim zsh tmux reattach-to-user-namespace go
 
 
 # ghq setup
+if ! has "go"; then
+    echo "golang is something wrong"
+    exit 1
+fi
 mkdir "$HOME/Scripts"
 export GOPATH="$HOME/Scripts"
+export PATH="$GOHOME/Scripts/bin:$PATH"
 echo '[ghq]\n\troot = $HOME/Scripts/src' >> $HOME/.gitconfig
 go get github.com/motemen/ghq
 
 
 # install zplug
+if ! has "zsh"; then
+    echo "zsh is something wrong"
+    exit 1
+fi
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 
 # download my dotfiles
+if ! has "ghq"; then
+    echo "ghq is something wrong"
+    exit 1
+fi
 ghq get https://github.com/dondakeshimo/dotfiles.git
 
 
