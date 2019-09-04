2ff8196ae6c72da8e06914a1afb69d6cec4936ab
Taku Shimomura <tashimom@yahoo-corp.jp>
2019-09-04 14:30:52 +0900

    [fix]remove reattachment-user-name-space from apt install

--- setup/requirements_install_apt.sh
+++ setup/requirements_install_apt.sh
@@ -16,7 +16,7 @@ sudo apt update
 sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
     libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
     xz-utils tk-dev libffi-dev liblzma-dev python-openssl git \
-    vim zsh tmux reattach-to-user-namespace golang fzy
+    vim zsh tmux golang fzy
 
 
 # ghq setup
