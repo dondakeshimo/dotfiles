7a22dbdf10ff676a2577e61aba63099d333d1151
dondakeshimo <went.went.takkun135@gmail.com>
2020-05-22 23:00:40 +0900

    feat: export before check pyenv command

--- .zsh/60_init_lang_mng.zsh
+++ .zsh/60_init_lang_mng.zsh
@@ -3,9 +3,9 @@
 ##############################
 
 # pyenv setting
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 if has "pyenv"; then
-    export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
 fi
 
