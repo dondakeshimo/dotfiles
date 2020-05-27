c157d0d8604021da6dce168032ecb3246352ca88
dondakeshimo <went.went.takkun135@gmail.com>
2020-05-27 19:12:37 +0900

    feat: add installing nerd-fonts and block name

--- setup/requirements_install_apt.sh
+++ setup/requirements_install_apt.sh
@@ -4,48 +4,66 @@ has() {
     type "${1:?too few arguments}" &>/dev/null
 }
 
-# confirm apt command exists
-if ! has "apt"; then
-    echo "apt is required"
-    exit 1
-fi
+: "Check apt" && {
+    if ! has "apt"; then
+        echo "apt is required" 1>&2
+        exit 1
+    fi
+}
 
 
-# install requirements via apt
-sudo apt update
-sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-    libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-    xz-utils tk-dev libffi-dev liblzma-dev python-openssl git \
-    vim zsh tmux golang fzy
+: "Install requirementes" && {
+    sudo apt update
+    sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+        libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+        xz-utils tk-dev libffi-dev liblzma-dev python-openssl git \
+        vim zsh tmux fzy
+
+    # install golang from ppa:longsleep because of version
+    sudo add-apt-repository ppa:longsleep/golang-backports
+    sudo apt update
+    sudo apt install golang-go
+}
 
 
-# TODO: fix go version to work ghq
-# ghq setup
-if ! has "go"; then
-    echo "golang is something wrong"
-    exit 1
-fi
-mkdir "$HOME/Scripts"
-export GOPATH="$HOME/Scripts"
-export PATH="$GOHOME/Scripts/bin:$PATH"
-go get github.com/motemen/ghq
-echo '[ghq]\n\troot = $HOME/Scripts/src' >> $HOME/.gitconfig
+: "Install ghq" && {
+    if ! has "go"; then
+        echo "golang is something wrong"
+        exit 1
+    fi
+
+    # go setting
+    mkdir "$HOME/Scripts"
+    export GOPATH="$HOME/Scripts"
+    export PATH="$GOHOME/Scripts/bin:$PATH"
+
+    # install ghq
+    go get github.com/motemen/ghq
+    echo '[ghq]\n\troot = $HOME/Scripts/src' >> $HOME/.gitconfig
+}
 
 
-# change shell zsh
-chsh -s $(which zsh)
+: "Use zsh" && {
+    chsh -s $(which zsh)
+}
 
 
-# install zplug
-if ! has "zsh"; then
-    echo "zsh is something wrong"
-    exit 1
-fi
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+: "Install zplug" && {
+    if ! has "zsh"; then
+        echo "zsh is something wrong"
+        exit 1
+    fi
+    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+}
 
 
-# TODO: nerd-fontのinstall
-# TODO: nerd font comaptible font のインストール
+: "Install nerd-fonts" && {
+    git clone -branch=master --depth 1 https://github.com/ryanoasis/nerd-fonts.git
+    cd nerd-fonts
+    ./install.sh
+    cd ..
+    rm -rf nerd-fonts
+}
 
 
 # success message
@@ -53,4 +71,3 @@ echo
 echo "Requirements installing is success!!"
 echo "Please restating shell"
 echo "And make symlink dotfiles by executing dotfiles/setup/symlink.sh"
-
