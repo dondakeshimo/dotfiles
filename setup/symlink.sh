11647e303421a9428fd1ccf0e85d10bc05100580
dondakeshimo <went.went.takkun135@gmail.com>
2020-09-20 22:52:42 +0900

    refactor: refactor symlink.sh

--- setup/symlink.sh
+++ setup/symlink.sh
@@ -3,22 +3,70 @@
 cd `dirname $0`/../
 
 
-FLAG_ZSH=false
-FLAG_VIM=false
-FLAG_TMUX=false
-FLAG_GIT=false
-FLAG_ATOM=false
-FLAG_BIN=false
-FLAG_SSH=false
-FLAG_YES=false
+: "Define global variables" && {
+    FLAG_EXEC=()
+    FLAG_BIN=false
+    FLAG_YES=false
+}
 
 
-function confirm_execution() {
+: "Check arguments" && {
+    if [ $# == 0 ]; then
+        echo "illegal arguments"
+        exit 1
+    fi
+}
+
+
+: "Parse arguments" && {
+    for OPT in "$@"
+    do
+        case $OPT in
+            'all' )
+                FLAG_EXEC=(".zshrc" ".zsh" ".vimrc" ".vim" ".tmux.conf" ".gitconfig" ".atom" ".ssh")
+                FLAG_BIN=true
+                ;;
+            'dotfiles' )
+                FLAG_EXEC=(".zshrc" ".zsh" ".vimrc" ".vim" ".tmux.conf" ".gitconfig" ".atom" ".ssh")
+                ;;
+            'zsh' )
+                FLAG_EXEC=(".zshrc" ".zsh")
+                ;;
+            'vim' )
+                FLAG_EXEC=(".vimrc" ".vim")
+                ;;
+            'tmux' )
+                FLAG_EXEC=(".tmux.conf")
+                ;;
+            'git' )
+                FLAG_EXEC=(".gitconfig")
+                ;;
+            'ssh' )
+                FLAG_EXEC=(".ssh")
+                ;;
+            'atom' )
+                FLAG_EXEC=(".atom")
+                ;;
+            'bin' )
+                FLAG_BIN=true
+                ;;
+            '-y' )
+                FLAG_YES=true
+                ;;
+        esac
+        shift
+    done
+}
+
+
+confirm() {
     if "$FLAG_YES"; then
         return 0
     fi
 
-    echo -n "$f already exists in $HOME. Do you overwrite it? (yes | no): "
+    text=${1}
+
+    echo -n "$text"
     read input
 
     if [ -z $input ] ; then
@@ -35,197 +83,52 @@ function confirm_execution() {
 }
 
 
-for OPT in "$@"
-do
-    case $OPT in
-        'all' )
-            FLAG_ZSH=true
-            FLAG_VIM=true
-            FLAG_TMUX=true
-            FLAG_GIT=true
-            FLAG_ATOM=true
-            FLAG_BIN=true
-            ;;
-        'dotfiles' )
-            FLAG_ZSH=true
-            FLAG_VIM=true
-            FLAG_TMUX=true
-            FLAG_GIT=true
-            FLAG_SSH=true
-            ;;
-        'zsh' )
-            FLAG_ZSH=true
-            ;;
-        'vim' )
-            FLAG_VIM=true
-            ;;
-        'tmux' )
-            FLAG_TMUX=true
-            ;;
-        'git' )
-            FLAG_GIT=true
-            ;;
-        'ssh' )
-            FLAG_SSH=true
-            ;;
-        'atom' )
-            FLAG_ATOM=true
-            ;;
-        'bin' )
-            FLAG_BIN=true
-            ;;
-        '-y' )
-            FLAG_YES=true
-            ;;
-    esac
-    shift
-done
+link_file() {
+    local entry=$1
+    local parent=${2:-$HOME}
+    local filename=${entry##*/}
+    local target=$parent/$filename
+    local text="$target is already exist. Are you sure to overwrite? ( yes | no ): "
+
+    echo "1 $entry $filename $parent"
+    if [ -d $entry ]; then
+        if [ ! -d $target ]; then mkdir $target; fi
+        echo "2 $entry $filename $parent"
+        for f in $entry/*; do
+            link_file $f $target
+        done
+    elif [ -f $target ]; then
+        if confirm "$text" ; then
+            echo "3 $entry $filename $parent"
+            ln -sf $PWD/$entry $parent
+            echo "$entry is overwrittened."
+        else
+            echo "4 $entry $filename $parent"
+            echo "$entry is skipped."
+        fi
+    else
+        echo "5 $entry $filename $parent"
+        ln -sf $PWD/$entry $parent
+        echo "$entry is linked."
+    fi
+}
 
 
 : "Link dotfiles" && {
-    for f in .??*
+    echo "${FLAG_EXEC[@]}"
+    for f in "${FLAG_EXEC[@]}"
     do
-        # ignore below files or directories
-        [[ "$f" == ".git" ]] && continue
-        [[ "$f" == ".DS_Store" ]] && continue
-        [[ "$f" == ".zsh" ]] && continue
-        [[ "$f" == ".ssh" ]] && continue
-        [[ "$f" == ".jupyter" ]] && continue
-
-        # check flag
-        [[ "$f" == ".zshrc" ]] && if ! "$FLAG_ZSH"; then continue; fi
-        [[ "$f" == ".vimrc" ]] && if ! "$FLAG_VIM"; then continue; fi
-        [[ "$f" == ".tmux.conf" ]] && if ! "$FLAG_TMUX"; then continue; fi
-        [[ "$f" == ".gitconfig" ]] && if ! "$FLAG_GIT"; then continue; fi
-
-        if [ -e $HOME/$f ]; then
-            if confirm_execution; then
-                ln -sf $PWD/$f $HOME
-                echo "$f is overwrittened."
-            else
-                echo "$f is skipped."
-                continue
-            fi
-        else
-            echo "$f is linked."
-            ln -sf $PWD/$f $HOME
-        fi
+        link_file $f $HOME
     done
 }
 
 
-: "Link .ssh" && {
-    if "$FLAG_SSH"; then
-        f=.ssh/config
-        if [ -e $HOME/$f ]; then
-            if confirm_execution; then
-                ln -sf $PWD/$f $HOME/.ssh
-                echo "$f is overwrittened."
-            else
-                echo "$f is skipped."
-            fi
-        else
-            if [ ! -d $HOME/.ssh ]; then
-                mkdir $HOME/.ssh
-            fi
-
-            ln -sf $PWD/$f $HOME/.ssh
-            echo "$f is linked."
-        fi
-    fi
-}
-
-
-: "Link zsh/" && {
-    if "$FLAG_ZSH"; then
-        for f in .zsh/*
-        do
-            [[ "$f" == ".git" ]] && continue
-            [[ "$f" == ".DS_Store" ]] && continue
-
-            if [ -e $HOME/$f ]; then
-                if confirm_execution; then
-                    ln -sf $PWD/$f $HOME/.zsh
-                    echo "$f is overwrittened."
-                else
-                    echo "$f is skipped."
-                    continue
-                fi
-            else
-                if [ ! -d $HOME/.zsh ]; then
-                    mkdir $HOME/.zsh
-                fi
-
-                ln -sf $PWD/$f $HOME/.zsh
-                echo "$f is linked."
-            fi
-        done
-    fi
-}
-
-
-: "Link Atom config" && {
-    if "$FLAG_ATOM"; then
-        for f in atom/*
-        do
-            [[ "$f" == ".git" ]] && continue
-            [[ "$f" == ".DS_Store" ]] && continue
-
-            if [ -e $HOME/$f ]; then
-                if confirm_execution; then
-                    ln -sf $PWD/$f $HOME/.atom
-                    echo "$f is overwrittened."
-                else
-                    echo "$f is skipped."
-                    continue
-                fi
-            else
-                if [ ! -d $HOME/.atom ]; then
-                    echo "atom is not installed"
-                    break
-                fi
-
-                echo "$f is linked."
-                ln -sf $PWD/$f $HOME/.atom
-            fi
-        done
-    fi
-}
-
-
-: "Link common binary" && {
-    if "$FLAG_BIN"; then
-        for f in bin/*
-        do
-            [[ "$f" == ".git" ]] && continue
-            [[ "$f" == ".DS_Store" ]] && continue
-            [ -d "$f" ] && continue
-
-            if [ -e $GOPATH/$f ]; then
-                if confirm_execution; then
-                    ln -sf $PWD/$f $GOPATH/bin
-                    echo "$f is overwrittened."
-                else
-                    echo "$f is skipped."
-                    continue
-                fi
-            else
-                if [ ! -d $GOPATH/bin ]; then
-                    mkdir $GOPATH/bin
-                fi
-
-                ln -sf $PWD/$f $GOPATH/bin
-                echo "$f is linked."
-            fi
-        done
-    fi
-}
-
-
-: "Link os specific binary" && {
+: "Link binary" && {
     if ! "$FLAG_BIN"; then exit 0;fi
+
     if "$FLAG_BIN" && [ $(uname) = 'Linux' ]; then ostype="linux"; fi
     if "$FLAG_BIN" && [ $(uname) = 'Darwin' ]; then ostype="osx"; fi
+
     if [ ! "{$GOPATH:+isdefined}" ]; then
         echo 'You must define $GOPATH'
         echo 'example:'
@@ -233,29 +136,20 @@ done
         exit 1
     fi
 
+    for f in bin/*
+    do
+        [[ "$f" == ".git" ]] && continue
+        [[ "$f" == ".DS_Store" ]] && continue
+        [ -d "$f" ] && continue
+
+        link_file $f $GOPATH/bin
+    done
+
     for f in bin/"$ostype"/*
     do
-        file=${f##*/}
-        [[ "$file" == ".git" ]] && continue
-        [[ "$file" == ".DS_Store" ]] && continue
+        [[ "$f" == ".git" ]] && continue
+        [[ "$f" == ".DS_Store" ]] && continue
 
-        if [ -e "$GOPATH/bin/$file" ]; then
-            echo "$GOPATH/bin/$file"
-            if confirm_execution; then
-                ln -sf $PWD/$f $GOPATH/bin
-                echo "$f is overwrittened."
-            else
-                echo "$f is skipped."
-                continue
-            fi
-        else
-            echo "$GOPATH/bin/$file"
-            if [ ! -d $GOPATH/bin ]; then
-                mkdir $GOPATH/bin
-            fi
-
-            ln -sf $PWD/$f $GOPATH/bin
-            echo "$f is linked."
-        fi
+        link_file $f $GOPATH/bin
     done
 }
