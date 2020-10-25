9a770bb16ad5831b2301a0b7079792517123635d
dondakeshimo <went.went.takkun135@gmail.com>
2020-10-25 17:58:30 +0900

    feat: add asdf setting

--- .zsh/60_init_lang_mng.zsh
+++ .zsh/60_init_lang_mng.zsh
@@ -40,3 +40,9 @@ if has "gcloud" && is_osx; then
     source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
     source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
 fi
+
+# asdf setting
+if [ -x $HOME/.asdf/asdf.sh ]; then
+    . $HOME/.asdf/asdf.sh
+    fpath=(${ASDF_DIR}/completion $fpath)
+fi
