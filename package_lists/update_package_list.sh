336adc186380af5be024c0c3396d05cf79f8003d
dondakeshimo <went.went.takkun135@gmail.com>
2024-10-14 16:48:40 +0900

    chore: remove package lists

--- package_lists/update_package_list.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd `dirname $0`/../
-
-apm list --installed --bare > package_lists/packages.txt
-
-[ -f Brewfile ] && rm Brewfile
-brew bundle dump && mv Brewfile package_lists/Brewfile
