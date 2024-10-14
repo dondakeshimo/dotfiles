6877d215d1acbfd2029b9cf6ea7b0bac50271018
Shimomura Taku <went.went.takkun135@gmail.com>
2024-10-14 18:55:59 +0900

    autopairs (#73)
    autoparisの追加とformatの実施

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -73,6 +73,7 @@ require("lazy").setup({
             vim.keymap.set("n", "gn", vim.lsp.buf.rename, { buffer = true })
             vim.keymap.set("n", "ga", vim.lsp.buf.code_action, { buffer = true })
             vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = true })
+            vim.keymap.set("n", "gf", vim.lsp.buf.format, { buffer = true })
             vim.keymap.set("n", "[g", vim.diagnostic.goto_prev, { buffer = true })
             vim.keymap.set("n", "]g", vim.diagnostic.goto_next, { buffer = true })
           end,
@@ -81,7 +82,7 @@ require("lazy").setup({
           function(server_name)
             require("lspconfig")[server_name].setup({
               capabilities = require('cmp_nvim_lsp').default_capabilities(
-              vim.lsp.protocol.make_client_capabilities()
+                vim.lsp.protocol.make_client_capabilities()
               ),
             })
           end,
@@ -234,8 +235,8 @@ require("lazy").setup({
         options = {
           icons_enabled = true,
           theme = 'auto',
-          component_separators = { left = '', right = ''},
-          section_separators = { left = '', right = ''},
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
           disabled_filetypes = {
             statusline = {},
             winbar = {},
@@ -250,18 +251,18 @@ require("lazy").setup({
           }
         },
         sections = {
-          lualine_a = {'mode'},
-          lualine_b = {'branch', 'diff', 'diagnostics'},
-          lualine_c = {'filename'},
-          lualine_x = {'encoding', 'fileformat', 'filetype'},
-          lualine_y = {'progress'},
-          lualine_z = {'location'}
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' }
         },
         inactive_sections = {
           lualine_a = {},
           lualine_b = {},
-          lualine_c = {'filename'},
-          lualine_x = {'location'},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
           lualine_y = {},
           lualine_z = {}
         },
@@ -377,6 +378,11 @@ require("lazy").setup({
     },
     {
       "machakann/vim-sandwich",
+    },
+    {
+      'windwp/nvim-autopairs',
+      event = "InsertEnter",
+      opts = {},
     },
     {
       "lukas-reineke/indent-blankline.nvim",
